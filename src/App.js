bd97dbfdc0b4a4d733d7ee5fa1fea2c754559c37
Mazdul1000 <mazdul1000@gmail.com>
2022-04-17 12:09:28 +0600

    set up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 
-import { Route,Routes } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import { Routes } from 'react-router-dom';
 import './App.css';
-import Home from './components/Home/Home/Home';
-import Login from './components/Login/Login/Login';
+
 import Header from './components/Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
     <Header></Header>
-    
+    <h1 className='text-primary'>Hello from Mazdul</h1>
     <Routes>
-      <Route path='/' element={<Home></Home>}></Route>
-      <Route path='login' element={<Login></Login>}></Route>
+
     </Routes>
+   
     </div>
   );
 }
--- src/components/Shared/Header/Header.js
+++ src/components/Shared/Header/Header.js
@@ -1,3 +1,4 @@
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import CustomLink from '../CustomLink/CustomLink';
 
@@ -6,9 +7,18 @@ const Header = () => {
 
 
     return (
-       <>
-       <h1 className=' text-6xl'> hello</h1>
-       </>
+     <header>
+          <Navbar bg="light" variant="light">
+    <Container>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link as={CustomLink} to='/login'>Home</Nav.Link>
+      <Nav.Link as={CustomLink} to='/login'>Home</Nav.Link>
+      <Nav.Link as={CustomLink} to='/login'>Home</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+     </header>
     );
 };
 
