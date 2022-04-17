4901f91bf09a807b7f117131c803438f3a4cfa47
Mazdul1000 <mazdul1000@gmail.com>
2022-04-17 21:14:07 +0600

    Update service compo.

--- src/components/Home/Services/Service/Service.js
+++ src/components/Home/Services/Service/Service.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
 import './Service.css';
 
-const Service = () => {
+const Service = ({service}) => {
+    const {name,price,session,description,img} = service;
     return (
-        <div>
-            
+        <div className='service mx-auto w-75 mt-5'>
+            <Card className="service mx-auto bg-dark text-white">
+  <Card.Img className='card-img w-100' src={img} alt="Card image" />
+  <Card.ImgOverlay className='img-overlay d-flex flex-column justify-content-center align-items-center'>
+    <Card.Title className='service-title'>{name}</Card.Title>
+    <Card.Text className='service-info px-4'>
+        {description}
+        </Card.Text>
+        <Card.Text className='service-session'>Appointment: {session} per month.</Card.Text>
+    <Card.Text className='service-price'>Price: ${price}/<small>mo</small> </Card.Text>
+    <Button className='book-btn btn btn-success'>Book Now</Button>
+  </Card.ImgOverlay>
+</Card>
         </div>
     );
 };
--- src/components/Home/Services/Services/Services.js
+++ src/components/Home/Services/Services/Services.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
+import './services.css';
 
 const Services = () => {
+
+    const [services,setServices] = useState([]);
+    useEffect(()=>{
+        fetch('service.json')
+        .then(res => res.json())
+        .then(data => setServices(data))
+
+    },[])
     return (
-        <div>
+        <div className='services-title mt-5 mb-2'>
             <h1>Services</h1>
+            {services.map(service => <Service key={service.id} service={service}></Service>)}
         </div>
     );
 };
