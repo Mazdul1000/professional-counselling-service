ee82cc87ae5c87f838596252fd8f259071aa1a6f
Mazdul1000 <mazdul1000@gmail.com>
2022-04-17 09:40:23 +0600

    modify

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>This is Our Home</h1>
+            <h1>This is not our home Our Home</h1>
         </div>
     );
 };
