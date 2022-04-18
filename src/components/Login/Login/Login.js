2e3928bfbd40d5e921f852098a88cebb1441292b
Mazdul1000 <mazdul1000@gmail.com>
2022-04-18 15:07:40 +0600

    updated login and register from

--- src/components/Login/Login/Login.js
+++ src/components/Login/Login/Login.js
@@ -1,98 +1,104 @@
 import './Login.css'
-import React, { useState,useRef } from 'react';
+
+import React, { useState, useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../../firebase.init';
-import {useSendPasswordResetEmail, useSignInWithEmailAndPassword} from 'react-firebase-hooks/auth'
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth'
 import { sendPasswordResetEmail } from 'firebase/auth';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import googleIcon from '../../../images/icons/google.svg';
 
 const Login = () => {
-    const emailRef = useRef('');
-    const passwordRef = useRef('');
-    const navigate = useNavigate();
-    const location = useLocation();
-     const from = location?.state?.from?.pathname || '/';
-     
-    
-    const [
-      signInWithEmailAndPassword,
-      user,
-      loading,
-      error,
-    ] = useSignInWithEmailAndPassword
-    (auth);
-    
-    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
-
-    if (loading || sending) {
-        return <h1>Loading</h1>
-    }
-
-    if (user) {
-        navigate(from, { replace: true });
-    }
-   
-    let errorElement;
-    if (error) {
-        errorElement = <p className='text-danger'>Error: {error?.message}</p>
-    }
+  const emailRef = useRef('');
+  const passwordRef = useRef('');
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location?.state?.from?.pathname || '/';
 
 
-    const handleFormSubmit = e => {
-  e.preventDefault();
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword
+      (auth);
+
+  const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+
+  if (loading || sending) {
+    return <h1>Loading</h1>
+  }
+
+  if (user) {
+    navigate(from, { replace: true });
+  }
+
+  let errorElement;
+  if (error) {
+    errorElement = <p className='text-danger'>Error: {error?.message}</p>
+  }
+
+
+  const handleFormSubmit = e => {
+    e.preventDefault();
     const email = emailRef.current.value;
-    const password = passwordRef.current.value;   
-    signInWithEmailAndPassword(email,password)
+    const password = passwordRef.current.value;
+    signInWithEmailAndPassword(email, password)
+  }
+  const navigateToRegister = () => {
+
+    navigate('/register');
+  }
+
+  const resetPassword = async () => {
+    const email = emailRef.current.value;
+    if (email) {
+      await sendPasswordResetEmail(email);
+      toast('Sent email');
     }
-    const navigateToRegister = () => {
+    else {
+      toast('please enter your email address');
+    }
+  }
+
+  return (
+    <div className='login w-100 mb-5'>
+
+  
+      <Form id='form' onSubmit={handleFormSubmit} className='w-50 mx-auto mt-5 border p-4 rounded'>
+      <h1 className='login-title fw-bold text-center mt-3'>Please Log In</h1>
+        <Button className='google-login-btn btn-success text-center d-flex justify-content-center align-items-center'><span><img src={googleIcon} alt="" /></span><span>Continue With Google</span></Button>
+
+        <div className='d-flex align-items-center justify-content-center mt-3 gap-3'><div className="divider w-25"></div> <h4 className='divider-text'>OR</h4> <div className="divider w-25"></div>  </div>
+
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+
+          <Form.Control ref={emailRef} className='user-input' type="email" placeholder="Enter email" required />
         
-        navigate('/register');
-    }
+        </Form.Group>
 
-    const resetPassword = async () => {
-        const email = emailRef.current.value;
-        if (email) {
-            await sendPasswordResetEmail(email);
-            toast('Sent email');
-        }
-        else{
-            toast('please enter your email address');
-        }
-    }
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          
+          <Form.Control className='user-input' ref={passwordRef} type="password" placeholder="Password" required />
+        </Form.Group>
+        {errorElement}
+       
+        <Button className='submit-btn d-block text-center' variant="success" type="submit">
+          Log In
+        </Button>
+ <p className='text-center mb-0 pass-reset d-flex justify-content-center align-items-center'><span> Forget Password?</span><button className='btn btn-link text-danger  text-decoration-none' onClick={resetPassword}>Reset Password</button> </p>
+      </Form>
 
-    return (
-        <div className='login w-100'>
-            <h1 className='text-primary fw-bold text-center mt-3'>Please Login</h1>
-            <Form onSubmit={handleFormSubmit} className='w-50 mx-auto mt-5 border p-4 rounded'>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control ref={emailRef} type="email" placeholder="Enter email"  required/>
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
+      <p className='toggle-btn fw-bold text-center fs-4'>New to <span>Lotus</span> ? <span className='text-danger' role="button" onClick={navigateToRegister}>Please register!</span></p>
 
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control ref={passwordRef} type="password" placeholder="Password" required/>
-  </Form.Group>
- {errorElement}
- <p>Forget Password? <button className='btn btn-link text-primary pe-auto text-decoration-none' onClick={resetPassword}>Reset Password</button> </p>
-  <Button variant="primary" type="submit">
-    Log In
-  </Button>
- 
-</Form>
+      <ToastContainer />
+    </div>
 
-<p className='fw-bold text-center mt-3 fs-4'>New to <span>Lotus</span> ? <span className='text-danger' role="button" onClick={navigateToRegister}>Please register!</span></p>
 
-<ToastContainer />
-        </div>
-
-            
-    );
+  );
 };
 
 export default Login;
--- src/components/Login/Register/Register.js
+++ src/components/Login/Register/Register.js
@@ -4,30 +4,32 @@ import { useCreateUserWithEmailAndPassword, useSendEmailVerification, useUpdateP
 import { useNavigate } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
 import auth from '../../../firebase.init';
+import googleIcon from '../../../images/icons/google.svg';
+import './Register.css'
 
 const Register = () => {
-
+    const navigate = useNavigate();
     const [
         createUserWithEmailAndPassword,
         user,
         loading,
         error,
-      ] = useCreateUserWithEmailAndPassword(auth, {sendEmailVerification: true});
-     const [updateProfile, updating, UpdateError] = useUpdateProfile(auth);
-       
-      let errorElement;
-      if (error) {
-          errorElement = <p className='text-danger'>Error: {error?.message}</p>
-      }
+    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+    const [updateProfile, updating, UpdateError] = useUpdateProfile(auth);
 
-      if(loading || updating){
+    let errorElement;
+    if (error) {
+        errorElement = <p className='text-danger'>Error: {error?.message}</p>
+    }
+
+    if (loading || updating) {
         return <h1>Loading</h1>
     }
 
 
-    const navigate = useNavigate();
 
-    const navigateToLogin = () =>{
+
+    const navigateToLogin = () => {
         navigate('/login');
     }
 
@@ -37,55 +39,52 @@ const Register = () => {
         const username = e.target.username.value;
         const email = e.target.email.value;
         const password = e.target.password.value;
-        createUserWithEmailAndPassword(email,password);
+        createUserWithEmailAndPassword(email, password);
         toast('generating account');
-        updateProfile({username});   
+        updateProfile({ username });
         console.log(user);
-        navigate('/home');
-        
+
     }
     return (
-        
-    
-    
-       
-            <div className='w-100'>
-            <h1 className='text-primary fw-bold text-center mt-3'>Please Register</h1>
-            <Form onSubmit={handleFormSubmit} className='w-50 mx-auto mt-5 border p-4 rounded'>
-    
-    
-    
-    
-    <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control  type="email" name="email" placeholder="Enter email"  required/>
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-    
-    <Form.Group className="mb-3">
-    <Form.Label>Username</Form.Label>
-    <Form.Control name="username" type="text" placeholder="Enter username"  required/>
-    </Form.Group>
-    
-    </Form.Group>
-    
-    
-    
-    <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control name="password" type="password" placeholder="Password" required/>
-    </Form.Group>
-    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-    </Form.Group>
-    {errorElement}
-    <Button variant="primary" type="submit">
-    Register
-    </Button>
-    </Form>
-    <p className='fw-bold text-center mt-3 fs-4'>Already have an account? <span className='text-danger' role="button" onClick={navigateToLogin}>Please Log In!</span></p>
-    <ToastContainer></ToastContainer>
+
+
+
+
+        <div className='w-100'>
+            <Form onSubmit={handleFormSubmit} id='form' className=' w-50 mx-auto mt-5 border p-4 rounded'>
+                <h1 className='login-title fw-bold text-center mt-3'>Please Register</h1>
+                <Button className='google-login-btn btn-success text-center d-flex justify-content-center align-items-center'><span><img src={googleIcon} alt="" /></span><span>Continue With Google</span></Button>
+                <div className='d-flex align-items-center justify-content-center mt-3 gap-3'><div className="divider w-25"></div> <h4 className='divider-text'>OR</h4> <div className="divider w-25"></div>  </div>
+
+                <Form.Group className="mb-3">
+
+                    <Form.Control className='user-input' name="username" type="text" placeholder="Enter username" required />
+
+
+                </Form.Group>
+
+                <Form.Group className="mb-3">
+
+                    <Form.Control className='user-input' type="email" name="email" placeholder="Enter email" required />
+
+                </Form.Group>
+
+
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+
+                    <Form.Control className='user-input' name="password" type="password" placeholder="Password" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                {errorElement}
+                <Button className='submit-btn btn-success d-block' variant="primary" type="submit">
+                    Register
+                </Button>
+            </Form>
+            <p className='fw-bold text-center mt-3 fs-4'>Already have an account? <span className='text-danger' role="button" onClick={navigateToLogin}>Please Log In!</span></p>
+            <ToastContainer></ToastContainer>
         </div>
     );
 };
